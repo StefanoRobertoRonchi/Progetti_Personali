61262e19e42b5a937d0ccff51b8ae089dabe47c8
StefanoRonchi <160749633+StefanoRobertoRonchi@users.noreply.github.com>
2025-12-31 23:36:11 +0100

    updated Bayesian_SSVS estimator with an explicit option to deal with the intercept
    Refactor Bayesian_MA_SSVS function parameters and add intercept option

--- BayesianMA/BMA_SSVS.py
+++ BayesianMA/BMA_SSVS.py
@@ -13,8 +13,12 @@ from scipy.stats import norm
 ################################################
 
 ################### Funzioni ################### 
-def Bayesian_MA_SSVS(y,X,n,burn_in,a=None,b=None,seed=42, identity = False, normalize_x = True,
-                    normalize_y = False):
+def Bayesian_MA_SSVS(y, X, 
+                    n: int=1000, burn_in: int=500,
+                    a: int=None, b: int=None, tau: int=None, c: int=None, p: float=None,
+                    identity: bool = False, add_intercept: bool = True,
+                    normalize_x: bool = True, normalize_y: bool = False,
+                    seed: int=42):
     """
     #### Hyperparameters priors and stating points ####
     ### Priors on Beta | Gamma ~ N_k(0, D_gamma*R*D_gamma) 
@@ -34,21 +38,26 @@ def Bayesian_MA_SSVS(y,X,n,burn_in,a=None,b=None,seed=42, identity = False, norm
         X = np.asarray(X)
     
     #____________________________________________________________
+    #### Aggiungo intercetta se richiesto
+    if add_intercept:
+        X = np.column_stack((np.ones(X.shape[0]), X))
+
+    #____________________________________________________________
     #### Normalizzazione dati se attiva (consigliata)
     
     if normalize_x:
         mu_X = np.mean(X,axis=0)
         sigma_X = np.std(X, ddof=1,axis=0)
-        ### Non standardizzo prima colonna che deve essere intercetta
-        sigma_X[0] = 1
-        mu_X[0] = 0
+        if add_intercept: ### Non standardizzo prima colonna che deve essere intercetta
+            sigma_X[0] = 1
+            mu_X[0] = 0
         X = (X - mu_X)/sigma_X
     if normalize_y:
         mu_y = np.mean(y,axis=0)
         sigma_y = np.std(y, ddof=1,axis=0)
         y = (y - mu_y)/sigma_y
     
-    
+
     #____________________________________________________________
     #### Setting parametri di partenza
     X_t = X.T
@@ -80,22 +89,29 @@ def Bayesian_MA_SSVS(y,X,n,burn_in,a=None,b=None,seed=42, identity = False, norm
         R  = R0 / np.outer(sX, sX) # Beta Correlation Matrix in the BMA model 
 
     
+    ### Inizializzo i parametri mancanti
+    # Sigma^2 prior parameters
     if (a is None) or (b is None): # Sigma IG prior parameters
         sigma_2_avg = sigma_OLS**2
         a = 6
         b = (a - 1)*sigma_2_avg
-        
-    ### Slab and spike parameter     
-    tau_sigma = 20 # scale parameter to shrink the starting value of the hyperparameter Tau (George McCulloch : rule of thumb = 10)
-    c = tau_sigma*10 # Standard Dev. slab multiplicative factor
-    tau_sq = (np.diag(var_beta_ols)/(tau_sigma**2)).ravel()  ## --> shrinkage parameter (Tau squared shrinked)
+
+    # Slab and spike parameter
+    if tau is None:
+        tau = 20 # scale parameter to shrink the starting value of the hyperparameter Tau (George McCulloch : rule of thumb = 10)    
+    if c is None:
+        c = tau*10 # Standard Dev. slab multiplicative factor   
+    if p is None:
+        p = 0.5 # Prior inclusion probability for each regressor
+    tau_sq = (np.diag(var_beta_ols)/(tau**2)).ravel()  ## --> shrinkage parameter (Tau squared shrinked)
+    
     for i in range(n):
         if i % 500 == 0:
             print(f"Iteration number {i}")
         #____________________________________________________________
         #### Estraggo i betas ~ N(Beta_post, V_beta_post)
         # Variance Beta structure 
-        a_beta_var = 1 + (c -1)*gammas  # Slab variance indicator, if the indicator gamma = 1
+        a_beta_var = 1 + (c-1)*gammas  # Slab variance indicator, if the indicator gamma = 1
         d = a_beta_var*np.sqrt(tau_sq) # beta prior variance is amplified by c^2
         D = np.diag(d)
         V_beta_post = np.linalg.inv(np.linalg.inv(D @ R @ D) + (X_t @ X)/sigma_sq)
@@ -110,12 +126,15 @@ def Bayesian_MA_SSVS(y,X,n,burn_in,a=None,b=None,seed=42, identity = False, norm
         sd_slab  = np.sqrt(tau_sq)*c
         sd_spike = np.sqrt(tau_sq)
 
-        log_num = norm.logpdf(Betas, loc=0.0, scale=sd_slab)
-        log_den = np.logaddexp(log_num, norm.logpdf(Betas, loc=0.0, scale=sd_spike))
+        log_num = p*norm.logpdf(Betas, loc=0.0, scale=sd_slab)
+        log_den = np.logaddexp(log_num, (1-p)*norm.logpdf(Betas, loc=0.0, scale=sd_spike))
 
         p_gamma1 = np.exp(log_num - log_den)
-        gammas[1:] = (unif[1:] < p_gamma1[1:]).astype(int)
-        gammas[0] = 1 # In ogni modello voglio l'intercetta
+        if add_intercept:
+            gammas[1:] = (unif[1:] < p_gamma1[1:]).astype(int)
+            gammas[0] = 1 # In ogni modello voglio l'intercetta
+        else:
+            gammas = (unif < p_gamma1).astype(int)
 
         #____________________________________________________________
         #### Estraggo sigma^2 posterior
@@ -161,34 +180,43 @@ def Bayesian_MA_SSVS(y,X,n,burn_in,a=None,b=None,seed=42, identity = False, norm
               "Betas": betas_final, 
               "Model Variance": sig_final}
 
+    output["added_intercept"] = add_intercept
+
     return output
 
-def PiP(BMA,X):
+def PiP(BMA, X):
     """
     Extract the Posterior Inclusion Probability of each regressor. The output is a pandas Series.
     """
     if isinstance(X,pd.DataFrame):
         regressors = X.columns
+        if BMA["added_intercept"]:
+            regressors = ["Intercept"] + list(regressors)
     else:
-        regressors = range(X.shape[1])
+        regressors = range(X.shape[1] + int(BMA["added_intercept"]))
 
     PIP = np.mean(BMA["Variable_Selected"], axis = 0) # Posterior Inclusion Probability
     return pd.Series(PIP, index = regressors)
 
 
-def TopModels(BMA,y,X, n=5, q_low = 0.25, q_high = 0.75):
+def TopModels(BMA, y, X, n_models: int=5):
     """
     Extract the list of most frequent models within the SSVS chain. For these promising combinations the R2 are computed for each model and 
-    aggregated to report the p25,median,mean,p95.
+    aggregated to report the p05,median,mean,p95.
     X must be the same used during the estimation. 
     
     """
     lista_modelli = pd.DataFrame(BMA["Variable_Selected"])
     beta_modelli = pd.DataFrame(BMA["Betas"])
+
     if isinstance(X,pd.DataFrame):
+        if BMA["added_intercept"]:
+            X = pd.concat([pd.DataFrame(np.ones(X.shape[0]), columns=['Intercept']), X], axis=1).copy()
         lista_modelli.columns = X.columns
         beta_modelli.columns = X.columns
     else:
+        if BMA["added_intercept"]:
+            X = np.column_stack((np.ones(X.shape[0]), X))
         lista_modelli.columns = range(X.shape[1])
         beta_modelli.columns = range(X.shape[1])
         
@@ -216,34 +244,36 @@ def TopModels(BMA,y,X, n=5, q_low = 0.25, q_high = 0.75):
           count="size"
       )
       .sort_values("count", ascending=False)
-      .head(n)
+      .head(n_models)
       .reset_index()
         )
     return model_selected
 
 ################### Esempio ################### 
 
-# Data test
-y_test = np.zeros(1000)
-X_test = np.zeros((1000,7))
+if __name__ == "__main__":
+    # Data test
+    y_test = np.zeros(1000)
+    X_test = np.zeros((1000,7))
 
-np.random.seed(2)
-n = 7
-A = np.random.randn(n, n)
-Sigma = A @ A.T                  # covariance PD
-D = np.sqrt(np.diag(Sigma))
-R = Sigma / np.outer(D, D)       # correlation matrix
-L = np.linalg.cholesky(R)
-sigma_eps = 0.2
+    np.random.seed(2)
+    n = 7
+    A = np.random.randn(n, n)
+    Sigma = A @ A.T                  # covariance PD
+    D = np.sqrt(np.diag(Sigma))
+    R = Sigma / np.outer(D, D)       # correlation matrix
+    L = np.linalg.cholesky(R)
+    sigma_eps = 0.2
 
-for i in range(1000):
-    X_test[i,:] = L @ np.random.standard_normal(size = 7)
-    y_test[i] = 0.2*X_test[i,0] + .3*X_test[i,2] + 0.5*X_test[i,6] + sigma_eps*np.random.standard_normal(size = 1)
-    
-print(pd.DataFrame(X_test).corr())
+    for i in range(1000):
+        X_test[i,:] = L @ np.random.standard_normal(size = 7)
+        y_test[i] = 0.2*X_test[i,0] + .3*X_test[i,2] + 0.5*X_test[i,6] + sigma_eps*np.random.standard_normal(size = 1)
+        
+    print(pd.DataFrame(X_test).corr())
 
-models_test = Bayesian_MA_SSVS(y=y_test,X=X_test,n=10000,burn_in=500,a=None,b=None,seed=42, identity = False)
-print(PiP(models_test,X_test))
-example = TopModels(models_test,y_test,X_test)
-example.columns = ["1","2","3","4","5","6","7","R2_mean","R2_median","R2_q05","R2_q95","count"]
-print(example)
+    models_test = Bayesian_MA_SSVS(y=y_test,X=X_test,n=10000,burn_in=500,a=None,b=None,seed=42, 
+                                   identity = False, add_intercept = False)
+    print(PiP(models_test,X_test))
+    example = TopModels(models_test,y_test,X_test)
+    example.columns = ["1","2","3","4","5","6","7","R2_mean","R2_median","R2_q05","R2_q95","count"]
+    print(example)
